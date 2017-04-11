50833df051816f2e01d7e9afdff64178da71aa55
Christopher Choi <cdddchris@gmail.com>
2017-04-11 13:30:10 -0700

    add parse

--- src/Distance.cr
+++ src/Distance.cr
@@ -1,11 +1,19 @@
-require "./Distance/*"
+require "./distance/*"
 require "option_parser"
 require "levenshtein"
 require "colorize"
 
+OptionParser.parse! do |parser|
+  parser.banner = "Usage: distance [word] [word]"
+  parser.on "-h", "--help", "Show this help" do
+    puts parser.colorize.green
+    exit
+  end
+end
+
 module Distance
   def self.run
-    puts "hello".colorize.red
+    puts "hello".colorize.blue
   end
 end
 
